3a47dcd45c066a03988851e754e27d18eb806bbe
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-03-14 22:31:52 +0100

    add UsersPage component

---
+++ frontend/src/pages/UsersPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UsersPage = () => {
+  // TODO add users table or list
+  return (
+    <div>
+      <h2>Users</h2>
+    </div>
+  );
+};
+
+export default UsersPage;
