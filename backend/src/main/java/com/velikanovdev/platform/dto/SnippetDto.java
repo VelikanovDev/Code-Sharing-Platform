a86620723a61e466a4a40b1da6e330f9e8884e6c
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-04-07 23:50:57 +0200

    BE: add pattern for LocalDateTime

--- backend/src/main/java/com/velikanovdev/platform/dto/SnippetDto.java
+++ backend/src/main/java/com/velikanovdev/platform/dto/SnippetDto.java
@@ -1,3 +1,14 @@
 package com.velikanovdev.platform.dto;
 
-public record SnippetDto (Long id, String code, String editDate, String date, UserDto user) { }
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.velikanovdev.platform.entity.Comment;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record SnippetDto (Long id,
+                          String code,
+                          @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss") LocalDateTime editDate,
+                          @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss") LocalDateTime date,
+                          UserDto user,
+                          List<Comment> comments) { }
