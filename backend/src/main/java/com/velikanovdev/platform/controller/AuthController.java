4aad57c0793375581fb853be81a978115b204559
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-04-19 15:02:56 +0200

    BE: change UserDto to UserAuthDetails

--- backend/src/main/java/com/velikanovdev/platform/controller/AuthController.java
+++ backend/src/main/java/com/velikanovdev/platform/controller/AuthController.java
@@ -1,9 +1,8 @@
 package com.velikanovdev.platform.controller;
 
 import com.velikanovdev.platform.config.UserAuthenticationProvider;
-import com.velikanovdev.platform.dto.CredentialsDto;
-import com.velikanovdev.platform.dto.SignUpDto;
-import com.velikanovdev.platform.dto.UserDto;
+import com.velikanovdev.platform.dto.UserAuthDetails;
+import com.velikanovdev.platform.dto.UserCredentials;
 import com.velikanovdev.platform.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,15 +21,15 @@ public class AuthController {
     private final UserAuthenticationProvider userAuthenticationProvider;
 
     @PostMapping("/login")
-    public ResponseEntity<UserDto> login(@RequestBody @Valid CredentialsDto credentialsDto) {
-        UserDto userDto = userService.login(credentialsDto);
-        userDto.setToken(userAuthenticationProvider.createToken(userDto));
-        return ResponseEntity.ok(userDto);
+    public ResponseEntity<UserAuthDetails> login(@RequestBody @Valid UserCredentials userCredentials) {
+        UserAuthDetails loggedInUser = userService.login(userCredentials);
+        loggedInUser.setToken(userAuthenticationProvider.createToken(loggedInUser));
+        return ResponseEntity.ok(loggedInUser);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UserDto> register(@RequestBody @Valid SignUpDto user) {
-        UserDto createdUser = userService.register(user);
+    public ResponseEntity<UserAuthDetails> register(@RequestBody @Valid UserCredentials userCredentials) {
+        UserAuthDetails createdUser = userService.register(userCredentials);
         createdUser.setToken(userAuthenticationProvider.createToken(createdUser));
         return ResponseEntity.created(URI.create("/users/" + createdUser.getId())).body(createdUser);
     }
