8c70e947490450fa27f8e72bf7393cd8e3b1ac02
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-04-19 14:59:07 +0200

    BE: change Snippet to SnippetDto

--- backend/src/main/java/com/velikanovdev/platform/service/PlatformService.java
+++ backend/src/main/java/com/velikanovdev/platform/service/PlatformService.java
@@ -1,5 +1,6 @@
 package com.velikanovdev.platform.service;
 
+import com.velikanovdev.platform.dto.SnippetDto;
 import com.velikanovdev.platform.entity.Comment;
 import com.velikanovdev.platform.entity.Snippet;
 
@@ -8,7 +9,7 @@ import java.util.List;
 public interface PlatformService {
     Snippet getSnippet(Long id);
     Long addOrUpdateSnippet(Snippet snippet);
-    List<Snippet> getLatest();
+    List<SnippetDto> getLatest();
     void deleteSnippet(Long id);
     void deleteAllSnippets();
     Comment addComment(Comment comment);
