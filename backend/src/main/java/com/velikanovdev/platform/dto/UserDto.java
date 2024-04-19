660a0749cdaee404df3f5125e2d2eb2ad1fc964e
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-04-19 16:21:33 +0200

    BE: add String role instead of entity

--- backend/src/main/java/com/velikanovdev/platform/dto/UserDto.java
+++ backend/src/main/java/com/velikanovdev/platform/dto/UserDto.java
@@ -1,9 +1,8 @@
 package com.velikanovdev.platform.dto;
 
 import com.velikanovdev.platform.entity.Snippet;
-import com.velikanovdev.platform.enums.Role;
 
 import java.util.List;
 
-public record UserDto(Long id, String username, Role role, List<Snippet> snippets){
+public record UserDto(Long id, String username, String role, List<Snippet> snippets){
 }
