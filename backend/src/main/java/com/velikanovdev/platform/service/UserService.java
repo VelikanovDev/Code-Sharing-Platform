ba7ec82891ef5b49ed646dffe0a533547671f03e
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-04-19 22:50:23 +0200

    BE: add UserService interface

--- backend/src/main/java/com/velikanovdev/platform/service/UserService.java
+++ backend/src/main/java/com/velikanovdev/platform/service/UserService.java
@@ -4,72 +4,12 @@ import com.velikanovdev.platform.dto.UserAuthDetails;
 import com.velikanovdev.platform.dto.UserCredentials;
 import com.velikanovdev.platform.dto.UserDto;
 import com.velikanovdev.platform.entity.User;
-import com.velikanovdev.platform.exception.AppException;
-import com.velikanovdev.platform.mappers.UserMapper;
-import com.velikanovdev.platform.repository.RoleRepository;
-import com.velikanovdev.platform.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-import java.nio.CharBuffer;
 import java.util.List;
-import java.util.Optional;
 
-@RequiredArgsConstructor
-@Service
-public class UserService {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final RoleRepository roleRepository;
-
-
-    public UserAuthDetails login(UserCredentials userCredentials) {
-        User user = userRepository.findByUsername(userCredentials.username())
-                .orElseThrow(() -> new AppException("Unknown user", HttpStatus.BAD_REQUEST));
-
-        if (passwordEncoder.matches(CharBuffer.wrap(userCredentials.password()), user.getPassword())) {
-            return UserMapper.INSTANCE.toUserAuthDetails(user);
-        }
-
-        throw new AppException("Invalid password", HttpStatus.BAD_REQUEST);
-    }
-
-    public UserAuthDetails register(UserCredentials userCredentials) {
-        Optional<User> optionalUser = userRepository.findByUsername(userCredentials.username());
-
-        if (optionalUser.isPresent()) {
-            throw new AppException("User with login " + userCredentials.username() + " already exists", HttpStatus.CONFLICT);
-        }
-
-        User user = UserMapper.INSTANCE.signUpToUser(userCredentials);
-
-        if(userRepository.findAll().isEmpty()) {
-            user.setRole(roleRepository.findByName("ADMIN")
-                    .orElseThrow(() -> new AppException("Role 'ADMIN' not found", HttpStatus.NOT_FOUND)));
-        }
-        else {
-            user.setRole(roleRepository.findByName("USER")
-                    .orElseThrow(() -> new AppException("Role 'USER' not found", HttpStatus.NOT_FOUND)));
-        }
-
-        user.setPassword(passwordEncoder.encode(CharBuffer.wrap(userCredentials.password())));
-
-        User savedUser = userRepository.save(user);
-
-        return UserMapper.INSTANCE.toUserAuthDetails(savedUser);
-    }
-
-    public List<UserDto> getAllUsers() {
-        List<User> users = userRepository.findAll();
-
-        return users.stream().map(UserMapper.INSTANCE::toUserDto).toList();
-    }
-
-    public User findByUsername(String username) {
-        return userRepository.findByUsername(username)
-                .orElseThrow(() -> new AppException("Unknown user", HttpStatus.NOT_FOUND));
-    }
-
+public interface UserService {
+    UserAuthDetails login(UserCredentials userCredentials);
+    UserAuthDetails register(UserCredentials userCredentials);
+    List<UserDto> getAllUsers();
+    User findByUsername(String username);
 }
