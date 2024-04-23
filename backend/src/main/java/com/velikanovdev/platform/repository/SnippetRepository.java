2882a9113cd18e0fa72e8a49abedaed0168db004
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-04-23 18:33:39 +0200

    BE: rename interface, delete annotation

--- backend/src/main/java/com/velikanovdev/platform/repository/SnippetRepository.java
+++ backend/src/main/java/com/velikanovdev/platform/repository/SnippetRepository.java
@@ -2,11 +2,9 @@ package com.velikanovdev.platform.repository;
 
 import com.velikanovdev.platform.entity.Snippet;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface PlatformRepository extends JpaRepository<Snippet, Long> {
+public interface SnippetRepository extends JpaRepository<Snippet, Long> {
     List<Snippet> findAllByOrderByDateDesc();
 }
