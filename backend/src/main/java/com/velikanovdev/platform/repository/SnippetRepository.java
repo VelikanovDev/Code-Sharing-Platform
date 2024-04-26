f6f8b99fc851671afa525d257e9947e5b4dda39b
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-04-27 00:36:23 +0200

    BE: add find methods to get comments and ratings by username

--- backend/src/main/java/com/velikanovdev/platform/repository/SnippetRepository.java
+++ backend/src/main/java/com/velikanovdev/platform/repository/SnippetRepository.java
@@ -1,10 +1,19 @@
 package com.velikanovdev.platform.repository;
 
+import com.velikanovdev.platform.entity.Comment;
+import com.velikanovdev.platform.entity.Rating;
 import com.velikanovdev.platform.entity.Snippet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface SnippetRepository extends JpaRepository<Snippet, Long> {
     List<Snippet> findAllByOrderByDateDesc();
+
+    @Query("SELECT c FROM Snippet s JOIN s.comments c WHERE c.username = :username")
+    List<Comment> findAllCommentsByUsername(String username);
+
+    @Query("SELECT r FROM Snippet s JOIN s.ratings r WHERE r.username = :username")
+    List<Rating> findAllRatingsByUsername(String username);
 }
