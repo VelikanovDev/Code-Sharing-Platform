7e5d06fb48bc8c728a25130376f353cf4939a3da
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-04-27 00:35:03 +0200

    BE: add Date formatter

--- backend/src/main/java/com/velikanovdev/platform/dto/CommentResponseDto.java
+++ backend/src/main/java/com/velikanovdev/platform/dto/CommentResponseDto.java
@@ -1,6 +1,10 @@
 package com.velikanovdev.platform.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
-public record CommentResponseDto(Long id, String username, String text, LocalDateTime date) {
-}
+public record CommentResponseDto(Long id,
+                                 String username,
+                                 String text,
+                                 @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss") LocalDateTime date) { }
